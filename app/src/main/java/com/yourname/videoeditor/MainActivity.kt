e7aaca99861cc4ca480183b0f9c7ad489ae6a337
Caleb Hillary <mayabihillary60@gmail.com>
2026-03-18 04:52:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yourname/videoeditor/MainActivity.kt
+++ app/src/main/java/com/yourname/videoeditor/MainActivity.kt
@@ -1,1 +1,11 @@
+package com.yourname.videoeditor
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
