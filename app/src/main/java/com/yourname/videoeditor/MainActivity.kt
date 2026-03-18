333fea8d47882b2a40a349bfc3c9600d99769332
Caleb Hillary <mayabihillary60@gmail.com>
2026-03-18 06:06:28 +0300

    Update MainActivity.kt

--- app/src/main/java/com/yourname/videoeditor/MainActivity.kt
+++ app/src/main/java/com/yourname/videoeditor/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yourname.videoeditor
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.yourname.videoeditor.databinding.ActivityMainBinding
+import com.yourname.videoeditor.feature.gallery.GalleryFragment
 
 class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        // Load gallery fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, GalleryFragment())
+            .commit()
     }
 }
